1ada703c945ba40750d5a770adf9b4ab1f27f50e
uSaiPrashanth <55424359+uSaiPrashanth@users.noreply.github.com>
2021-10-28 14:50:41 +0530

    algorithm modification

--- algorithm.py
+++ algorithm.py
@@ -1,15 +1,9 @@
 def compare(field1,field2):
     '''Merge if both are equal'''
-    if((field1 is not None) and (field2 is not None)):
-        if(field1 == field2):
-            return field1,"NA"
-        else:
-            return field1,field2
-    if(field1 is None):
-        field1 = "NA"
-    if(field2 is None):
-        field2 = "NA"
-    return field1,field2
+    if((field1 is not None) and (field2 is not None) and (field1 == field2)):
+        return field1,None
+    else:
+        return field1,field2
 
 def simplify(
     house=None,
@@ -23,24 +17,26 @@ def simplify(
     pincode=None
 ):
     '''Algorithm to simplify address fields'''
+    
     locality,landmark = compare(locality,landmark)
     village,subdistrict = compare(village,subdistrict)
-    if(subdistrict != "NA"):
+    if(subdistrict is not None):
         subdistrict,district = compare(subdistrict,district)
-    if(district != "NA"):
+    if(district is not None):
         district,state = compare(district,state)
-    print([house, street, locality,
-        landmark, village, subdistrict,
-        district, state, pincode])
-    address = " ".join(
-        [house, street, locality,
-        landmark, village, subdistrict,
-        district, state, pincode]
+    
+
+    input_data = [house, street, locality, landmark, village, subdistrict, district, state, pincode]
+    for i in range(len(input_data)):
+        if(input_data[i] is None):
+            input_data[i] = 'NA'
+        
+    address = " ".join(input_data)
     return address
 if __name__ == '__main__':
-    for i in ['one','two',None]:
-        for j in ['one','two',None]:
-            print(i,j,compare(i,j))
-    print(simplify())
+    # for i in ['one','two',None]:
+    #     for j in ['one','two',None]:
+    #         print(i,j,compare(i,j))
+    print(simplify("one","two","three","three","five","five","eight","eight","nine"))
 
 

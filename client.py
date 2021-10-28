059747165c3c263462bdadd185b11ce2b9058212
uSaiPrashanth <55424359+uSaiPrashanth@users.noreply.github.com>
2021-10-28 15:14:54 +0530

    ngrok port

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 import requests
-res = requests.post('http://localhost:5000/',json={
+res = requests.post('https://7b2a-2409-4070-2d4a-7b3d-c4f8-c563-911d-e403.ngrok.io',json={
     'house':'B-102 Sri Shiridi Sai Enclave',
     'street':'Street no 9',
     'locality':'Gayathri Nagar',

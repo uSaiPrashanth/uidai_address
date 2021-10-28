4cab9972d238cca696ae81cce8774a6ba74a8509
uSaiPrashanth <55424359+uSaiPrashanth@users.noreply.github.com>
2021-10-28 15:39:17 +0530

    added logging

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
 # Creates a simple API
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 from flask import Flask,request,json
 from algorithm import simplify
@@ -20,7 +37,7 @@ def hello_world():
     for field in fields:
         assert field in data
     address = simplify(**data)
-    app.logger.info(f'Address processed: {address}')
+    app.logger.info(f'[Address processed] : {address}')
     return {
         'address':address
     }
